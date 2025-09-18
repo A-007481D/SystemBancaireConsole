e5843cc43a25caac56d4346bcd8339ccb1139f96
A-007481D <labidabdelmalek@gmail.com>
2025-09-18 20:06:52 +0100

    feat(view): add read methodes for the view class

--- src/service/BanqueService.java
+++ src/service/BanqueService.java
@@ -14,6 +14,8 @@ public class BanqueService {
 
     }
 
-    public
+    public Compte c createAccountFlow() {
+
+    }
 
 }
--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -16,14 +16,32 @@ public class ConsoleView {
         return readInt();
     }
 
-    public int showAccountMenu() {
-        System.out.println("--- MENU PERSONEL ---");
+    public int showAccountMenu(String code) {
+        System.out.println("--- MENU PERSONEL (" + code + ") ---");
         System.out.println("1. Versement");
         System.out.println("2. retraite");
         System.out.println("3. transfert");
         System.out.println("4. consulter solde");
         System.out.println("5. lister les operations");
+        System.out.println("choix : ");
         return readInt();
+    }
+
+    public String readLIne(String prompt) {
+        System.out.println(prompt);
+        return sc.nextLine().trim();
+    }
+
+    public double readDouble(String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                String s = sc.nextLine().trim();
+                return Double.parseDouble(s);
+            } catch (Exception e) {
+                System.out.println("choix invalid.");
+            }
+        }
     }
 
 
